6cf86ee026be10cb3def22a109ebc1f867f0f15e
Craigc <craigclement@hotmail.com>
2021-04-20 16:04:27 +0100

    filteredArray

--- apps.js
+++ apps.js
@@ -3,44 +3,73 @@ const scoreDisplay = document.querySelector('.score')
 const startBtn = document.querySelector('button')
 
 const width = 9
-const cells = []
-let spaceShip = 76
-const spaceInvader = 0
+const cells = [] //cells is an array of divs!
+let spaceShip = 76 //player starting postion
 let laser = ''
 let playerScore = 0
+const hitInvaders = []
 
-
+//add divs to cells array via the the DOM
 for (let i = 0; i < (width) ** 2; i++) {
   const div = document.createElement('div')
   grid.appendChild(div)
   div.innerHTML = i
   cells.push(div)
 }
-
+// add player postion at start of game
 cells[spaceShip].classList.add('spaceShip')
 
-let invaders = [0,1,2,3,4,5]
+//array of invaders
+let invaders = [1,2,3,4,5,6,7,10,11,12,13,14,15,16]
 
+//function to loop through invaders array and add spaceInvader class to the divs within the cells array
 function addInvaders() {
   invaders.forEach(invader => cells[invader].classList.add('spaceInvader'))
 }
-
+//function to loop through invaders array and remove spaceInvader class from the divs within the cells array
 function removeInvaders() {
   invaders.forEach(invader => cells[invader].classList.remove('spaceInvader'))
 }
-
+//function to loop through invaders array and move spaceInvader one div to the right
 function moveInvaders() {
   invaders = invaders.map(invader => invader + 1) 
+  if (invaders === cells[72]){
+    console.log('Game Over')
+  }
+}
+//function to generate invader bombs at random postions, dropped every 1sec if cells conatins the invader class
+function dropBomb() {
+  setInterval(() => {
+    const randomInvader = Math.floor(Math.random() * cells.length)
+    if (cells[randomInvader].classList.contains('spaceInvader')) {
+      cells[randomInvader].classList.add('bomb')
+    }
+  },1000)
 }
 
-addInvaders()
+//function to filter invader from invaders array and return a new invaders when cell index is not equal to laser index
+// function hitInvader() {
+//   const filteredInvaders = invaders.filter((invaders) => {
+//     return invaders !== cells[laser] //retun if condition true
+//   })
+//   return filteredInvaders
+  
+// } 
+// hitInvader()
 
+
+//add invaders to grid
+addInvaders()
+dropBomb()
+
+// calling all invader functions within setInterval to move them all accross the cells array one div at a time
 setInterval(() => {
   removeInvaders()
   moveInvaders()
   addInvaders()
 
 },1000)
+
 
 // Player Movement + player fire laser
 document.addEventListener('keydown', (event) => {
@@ -58,6 +87,8 @@ document.addEventListener('keydown', (event) => {
   if (key === ' ') {
     laser = spaceShip - width
     cells[laser].classList.add('laser')
+    
+
     const intervalID = setInterval(() => {
       // remove laser class once out of playing area 
       if (laser < width) {
@@ -72,20 +103,28 @@ document.addEventListener('keydown', (event) => {
 
       //collision of laser and invader
       const hitIndex = invaders.find(invader => invader === laser)
-
+      // if hit index not the same as laser, do nothing.
       if (!hitIndex) return 
-      console.log('hit!')
+      console.log(hitIndex) 
+      //else....
+    
+      invaders = invaders.filter((invader => {
+        return invader !== hitIndex
+      }))
+
+      
+
+
 
       cells[hitIndex].classList.remove('spaceInvader')
       cells[hitIndex].classList.remove('laser')
+
       //stop laser from continuing up the grid       
-      clearInterval(intervalID)
-      //add 10pts to player score when invader hit.        
+      clearInterval(intervalID)     
       playerScore = playerScore + 10
-      console.log(playerScore)
       scoreDisplay.innerHTML = (`Player Score: ${playerScore}`)
-      //need to remove invader once 'hit' 
-    
     }, 200)
   }
 })
+
+
